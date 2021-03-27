8046cfac4838104e1faf86cf138e199c268f0b18
Steven Flintham <sgf@lemma.co.uk>
2021-03-27 02:01:14 +0000

    Start refactoring into multiple files

--- make.sh
+++ make.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # TODO: A simple but "real" Makefile to avoid building all in one go every time
-gcc -o abe-util -Wall -g --std=c99 src/main.c src/lib6502.c src/cargs.c
+gcc -o abe-util -Wall -g --std=c99 src/main.c src/other.c src/lib6502.c src/cargs.c
