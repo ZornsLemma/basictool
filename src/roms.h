626c875186cf46cc6fcec3db2b407dd1f3a658d5
Steven Flintham <sgf@lemma.co.uk>
2021-04-02 16:54:47 +0100

    Tweak TODO

--- src/roms.h
+++ src/roms.h
@@ -9,7 +9,8 @@ enum {
     bank_basic    = 12  // same as Master 128, although it's arbitrary really
 };
 
-// TODO: Rename rom_editor to rom_abe? Ditto bank_editor
+// TODO: Rename rom_editor to rom_abe? Ditto bank_editor. Or maybe I should
+// be calling it "bet", *HELP calls it "BASIC Editor & Toolkit".
 extern const uint8_t rom_editor_a[];
 extern const uint8_t rom_editor_b[];
 extern const uint8_t rom_basic[];
