ZornsLemma/basictool

commit f38c1ac046c5ddd25d4480d412b8ecdaf0cbd98b

Add cr/lf constants, miscellaneous tidying

src/emulation.c
@@ -103,7 +103,9 @@ NORETURN static int callback_abort_call(M6502 *mpu, uint16_t address, uint8_t da
     callback_abort("call", address, data);
 }
 
-static int callback_return_via_rts() {
+// Pull an RTS-style address (i.e. target-1) from the emulated machine's stack
+// and return the target address.
+static int pull_rts_target() {
     uint16_t address = mpu_read_u16(0x101 + mpu_registers.s);
     mpu_registers.s += 2;
     address += 1;
@@ -116,20 +118,18 @@ static int callback_osrdch(M6502 *mpu, uint16_t address, uint8_t data) {
 }
 
 static int callback_oswrch(M6502 *mpu, uint16_t address, uint8_t data) {
-    int c = mpu_registers.a;
-    driver_oswrch(c);
-    return callback_return_via_rts(mpu);
+    driver_oswrch(data);
+    return pull_rts_target(mpu);
 }
 
 static int callback_osnewl(M6502 *mpu, uint16_t address, uint8_t data) {
-    driver_oswrch(0xa);
-    driver_oswrch(0xd);
-    return callback_return_via_rts(mpu);
+    driver_oswrch(lf);
+    driver_oswrch(cr);
+    return pull_rts_target(mpu);
 }
 
 static int callback_osasci(M6502 *mpu, uint16_t address, uint8_t data) {
-    int c = mpu_registers.a;
-    if (c == 0xd) {
+    if (mpu_registers.a == cr) {
         return callback_osnewl(mpu, address, data);
     } else {
         return callback_oswrch(mpu, address, data);
@@ -138,13 +138,13 @@ static int callback_osasci(M6502 *mpu, uint16_t address, uint8_t data) {
 
 static int callback_osbyte_return_x(uint8_t x) {
     mpu_registers.x = x;
-    return callback_return_via_rts(mpu);
+    return pull_rts_target(mpu);
 }
 
 static int callback_osbyte_return_u16(uint16_t value) {
     mpu_registers.x = value & 0xff;
     mpu_registers.y = (value >> 8) & 0xff;
-    return callback_return_via_rts(mpu);
+    return pull_rts_target(mpu);
 }
 
 static int callback_osbyte_read_vdu_variable(void) {
@@ -159,17 +159,17 @@ static int callback_osbyte_read_vdu_variable(void) {
     }
     mpu_registers.x = vdu_variables[i];
     mpu_registers.y = vdu_variables[i + 1];
-    return callback_return_via_rts(mpu);
+    return pull_rts_target(mpu);
 }
 
 static int callback_osbyte(M6502 *mpu, uint16_t address, uint8_t data) {
     switch (mpu_registers.a) {
         case 0x03: // select output device
-            return callback_return_via_rts(mpu); // treat as no-op
+            return pull_rts_target(mpu); // treat as no-op
         case 0x0f: // flush buffers
-            return callback_return_via_rts(mpu); // treat as no-op
+            return pull_rts_target(mpu); // treat as no-op
         case 0x7c: // clear Escape condition
-            return callback_return_via_rts(mpu); // treat as no-op
+            return pull_rts_target(mpu); // treat as no-op
         case 0x7e: // acknowledge Escape condition
             return callback_osbyte_return_x(0); // no Escape condition pending
         case 0x83: // read OSHWM
@@ -184,7 +184,7 @@ static int callback_osbyte(M6502 *mpu, uint16_t address, uint8_t data) {
             // might be nice to emulate this properly, but it also seems silly
             // to complicate the I/O emulation further when we can simply
             // do this explicitly.
-            return callback_return_via_rts(); // treat as no-op
+            return pull_rts_target(); // treat as no-op
         case 0xa0:
             return callback_osbyte_read_vdu_variable();
         default:
@@ -423,7 +423,7 @@ void execute_osrdch(const char *s) {
           "Internal error: Emulated machine isn't waiting for OSRDCH");
     mpu_registers.a = s[0];
     mpu_clear_carry(mpu); // no error
-    mpu_registers.pc = callback_return_via_rts(mpu);
+    mpu_registers.pc = pull_rts_target(mpu);
     mpu_run();
 } 
 
@@ -443,12 +443,12 @@ void execute_input_line(const char *line) {
     for (int i = 0; i < pending_length; ++i) {
         driver_oswrch(line[i]);
     }
-    driver_oswrch(0xa); driver_oswrch(0xd);
+    driver_oswrch(lf); driver_oswrch(cr);
 
-    mpu_memory[buffer + pending_length] = 0xd;
+    mpu_memory[buffer + pending_length] = cr;
     mpu_registers.y = pending_length;
     mpu_clear_carry(mpu); // input not terminated by Escape
-    mpu_registers.pc = callback_return_via_rts(mpu);
+    mpu_registers.pc = pull_rts_target(mpu);
     mpu_run();
 }
 

src/driver.c
@@ -263,7 +263,7 @@ static void type_basic_program(char *data, size_t length) {
     // Ensure that the last line of the data is terminated by a carriage
     // return, taking advantage of the extra byte allocated by load_binary() to
     // know this is safe.
-    data[length] = 0x0d;
+    data[length] = cr;
 
     // As with beebasm's PUTBASIC, line numbers are optional on the input. We
     // auto-assign line numbers; line numbers in the input are recognised and

src/utils.c
@@ -128,7 +128,7 @@ char *get_line(char **data_ptr, size_t *length_ptr) {
 
     // Find the end of the line.
     char *eol = data;
-    while ((*eol != 0x0d) && (*eol != 0x0a)) {
+    while ((*eol != cr) && (*eol != lf)) {
         ++eol; --length;
     }
     assert(length > 0);
@@ -140,7 +140,7 @@ char *get_line(char **data_ptr, size_t *length_ptr) {
     char *next_line = eol;
     if (length > 0) {
         ++next_line;
-        const char opposite_terminator = (terminator == 0x0d) ? 0x0a: 0x0d;
+        const char opposite_terminator = (terminator == cr) ? lf: cr;
         if (*next_line == opposite_terminator) {
             ++next_line; --length;
         }

src/utils.h
@@ -13,6 +13,20 @@
 #define NORETURN
 #endif
 
+// Constants for carriage return (CR) and line feed (LF). These are short
+// names but we use them a lot so let's just pollute the global namespace
+// with them.
+//
+// We could probably just use '\r' and '\n' because we're already
+// assuming the C environment uses ASCII, but let's be paranoid - I could
+// vaguely imagine an ASCII C environment where '\r' is 10 and '\n' is
+// 13 because it makes writing to native text files with CR line edings
+// via the C library less of a special case. (The draft C99 standard
+// (http://www.open-std.org/jtc1/sc22/wg14/www/docs/n1256.pdf) says the value
+// of these escape sequences is implementation-defined; see section 5.2.2.3.)
+static const char cr = 13;
+static const char lf = 10;
+
 // TODO: COMMENT IF KEEP
 extern int error_line_number;
 

commit 460aeaa196432ff94969c57faecec09dabd41d36

Fix bug introduced in f38c1ac046c5ddd25d4480d412b8ecdaf0cbd98b
D'oh!

src/emulation.c
@@ -118,7 +118,7 @@ static int callback_osrdch(M6502 *mpu, uint16_t address, uint8_t data) {
 }
 
 static int callback_oswrch(M6502 *mpu, uint16_t address, uint8_t data) {
-    driver_oswrch(data);
+    driver_oswrch(mpu_registers.a);
     return pull_rts_target(mpu);
 }
 