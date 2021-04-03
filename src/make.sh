d76a099916f63c6d88deb17d020cffdb2daeb125
Steven Flintham <sgf@lemma.co.uk>
2021-04-03 03:14:37 +0100

    Update make.sh

--- src/make.sh
+++ src/make.sh
@@ -1,13 +1,22 @@
 #!/bin/bash
 
-set -e
+# This is a really simple shell script to perform the build. The idea is that
+# it may be helpful on non-Unix systems where the makefile doesn't work; it
+# probably won't run on those machines, but it should be easy to translate into
+# the native equivalent.
 
-# TODO: Try building with clang at some point
-# TODO: Keep this up to date with Makefile
-# TODO: A simple but "real" Makefile to avoid building all in one go every time
+# Stop if any errors occur.
+set -e
+# Show commands as they execute.
+set -v
+
+# Generate C-style hex dumps of the ROM images, so they can be compiled into
+# the executable. roms.c #includes these auto-generated files.
 gcc -o bintoinc -Wall -g --std=c99 bintoinc.c
 ./bintoinc ../roms/EDITORA100.rom > zz-editor-a.c
 ./bintoinc ../roms/EDITORB100.rom > zz-editor-b.c
 ./bintoinc ../roms/basic4.rom > zz-basic.c
 
-gcc -o ../basictool -Wall -g --std=c99 main.c other.c data.c lib6502.c cargs.c
+gcc -o ../basictool -g -O2 -Wall -Werror --std=c99 main.c config.c emulation.c driver.c roms.c utils.c lib6502.c cargs.c
+
+# vi: colorcolumn=80
