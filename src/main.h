713e08036a8c91470642e52c5404df4648d7081a
Steven Flintham <sgf@lemma.co.uk>
2021-04-03 02:43:10 +0100

    Add missing main.h
    Oops! Luckily this doesn't change all that much.

---
+++ src/main.h
@@ -0,0 +1,13 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+// The name of this program, derived from argv[0].
+extern const char *program_name;
+
+// filenames[0] is the input filename, filenames[1] is the output
+// filename. stdin/stdout are represented as null.
+extern const char *filenames[2];
+
+// vi: colorcolumn=80
+
+#endif
