a585d4a5242496514b75889bf32260217ebab0f1
Steven Flintham <sgf@lemma.co.uk>
2021-04-04 02:55:15 +0100

    Prepare for 0.02 release tag
    scruss has confirmed the gcc 10 build problem is now fixed.

--- src/main.c
+++ src/main.c
@@ -42,7 +42,7 @@
 #include "roms.h"
 #include "utils.h"
 
-#define VERSION "0.02-pre"
+#define VERSION "0.02"
 
 // We could just sum the bool variables, but let's play it safe in case we
 // want to support pre-C99 compilers where bool is a typedef for something
