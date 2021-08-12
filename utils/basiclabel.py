f997c0a330afaf55f0953faea4058a91bec51a46
Steven Flintham <sgf@lemma.co.uk>
2021-08-12 17:08:11 +0100

    Exempt string literals from label substitution

--- utils/basiclabel.py
+++ utils/basiclabel.py
@@ -48,6 +48,42 @@ def find_label_definition(line):
     return (label, user_content)
 
 
+# Return a list of consecutive substrings in 'line' such that even-numbered
+# elements of the list are BASIC code and odd-numbered elements are string
+# literals.
+def break_string_at_literals(line):
+    in_literal = False
+    i = 0
+    output = []
+    fragment = ""
+    while i < len(line):
+        c = line[i]
+        if c == '"':
+            if not in_literal:
+                in_literal = not in_literal
+                output.append(fragment)
+                fragment = ""
+                fragment += c
+                i += 1
+            else:
+                if (i + 1) < len(line) and line[i + 1] == '"':
+                    # This is an escaped quote within the literal.
+                    fragment += '""'
+                    i += 2
+                else:
+                    fragment += c
+                    i += 1
+                    in_literal = not in_literal
+                    output.append(fragment)
+                    fragment = ""
+        else:
+            fragment += c
+            i += 1
+    if len(fragment) > 0:
+        output.append(fragment)
+    return output
+
+
 def find_label_reference(line):
     # TODO: Needs to be smarter about literal strings in line
     reference_list = re.findall(r"%%[A-Za-z0-9_]+%%", line)
@@ -58,6 +94,17 @@ def find_label_reference(line):
     start_index = line.index(reference)
     end_index = start_index + len(reference)
     return (label, start_index, end_index)
+
+
+# TODO: DELETE
+if False:
+        print(break_string_at_literals('PRINT 5'))
+        print(break_string_at_literals('PRINT 5, "Hello", 20'))
+        print(break_string_at_literals('PRINT "Hello"'))
+        print(break_string_at_literals('PRINT "He""llo"'))
+        print(break_string_at_literals('PRINT "He""""llo"'))
+        print(break_string_at_literals('"He""""llo"'))
+        print(break_string_at_literals('PRINT "He"llo"'))
 
 
 parser = argparse.ArgumentParser(description='Preprocess text BBC BASIC to allow use of labels instead of line numbers.\n\nUse "%%LABELNAME%%:" at the start of a line to define a label and "%%LABELNAME%%" to refer to a label.')
@@ -107,20 +154,29 @@ with my_open(cmd_args.input_file, "r") as f:
 # error occurs.
 output = []
 for internal_line_number, (user_line_number, user_content) in enumerate(program):
-    while True:
-        label_reference, start_index, end_index = find_label_reference(user_content)
-        if label_reference is None:
-            break
-        if label_reference == "INCREMENT":
-            label_value = auto_line_number_increment
-        elif label_reference in label_internal_line:
-            label_internal_line_number = label_internal_line[label_reference]
-            label_user_line_number = program[label_internal_line_number][0]
-            label_value = label_user_line_number
-        else:
-            die_input("unrecognised label '%s'" % label_reference)
-        user_content = user_content[:start_index] + str(label_value) + user_content[end_index:]
-    output.append("%d%s" % (user_line_number, user_content))
+    user_content_list = break_string_at_literals(user_content)
+    output_line = ""
+    while len(user_content_list) > 0:
+        user_content_fragment = user_content_list.pop(0)
+        while True:
+            label_reference, start_index, end_index = find_label_reference(user_content_fragment)
+            if label_reference is None:
+                break
+            if label_reference == "INCREMENT":
+                label_value = auto_line_number_increment
+            elif label_reference in label_internal_line:
+                label_internal_line_number = label_internal_line[label_reference]
+                label_user_line_number = program[label_internal_line_number][0]
+                label_value = label_user_line_number
+            else:
+                die_input("unrecognised label '%s'" % label_reference)
+            user_content_fragment = user_content_fragment[:start_index] + str(label_value) + user_content_fragment[end_index:]
+
+        output_line += user_content_fragment
+        if len(user_content_list) > 0:
+            output_line += user_content_list.pop(0)
+
+    output.append("%d%s" % (user_line_number, output_line))
 
 with my_open(cmd_args.output_file, "w") as f:
     print("\n".join(output), file=f)
