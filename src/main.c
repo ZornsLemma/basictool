47bd1f68d3cd040a248cb8a612fc228222c081b4
Steven Flintham <sgf@lemma.co.uk>
2021-10-22 17:34:09 +0100

    Bump version to 0.09-pre

--- src/main.c
+++ src/main.c
@@ -45,7 +45,7 @@
 #include "roms.h"
 #include "utils.h"
 
-#define VERSION "0.08"
+#define VERSION "0.09-pre"
 
 // We could just sum the bool variables, but let's play it safe in case we
 // want to support pre-C99 compilers where bool is a typedef for something
