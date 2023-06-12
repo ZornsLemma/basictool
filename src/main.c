d64f4065d1a593e9c6377f56546e8e2645e8c35d
Steven Flintham <sgf@lemma.co.uk>
2023-06-12 20:49:14 +0100

    Bump version after tagging

--- src/main.c
+++ src/main.c
@@ -48,7 +48,7 @@
 #include <fcntl.h>
 #endif
 
-#define VERSION "0.10"
+#define VERSION "0.11-pre"
 
 // We could just sum the bool variables, but let's play it safe in case we
 // want to support pre-C99 compilers where bool is a typedef for something
